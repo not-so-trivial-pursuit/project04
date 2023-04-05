7cc00e0e4bc6ecaf436d12394ac7f88e0a6d675b
Dae Heum Na <daeheumna@gmail.com>
2023-04-04 22:33:38 -0400

    populated radio inputs with api pulls in currentGame.js

--- src/components/CurrentGame.js
+++ src/components/CurrentGame.js
@@ -1,27 +1,72 @@
 // CurrentGame.js
 import { useState, useEffect } from 'react';
 
+
+
 const CurrentGame = (props) => {
+
+    const [ userSelection, setUserSelection ] = useState(null)
+
+    const handleClick = (e) => {
+        setUserSelection(e.target.value)
+    }
+
+
     return(
         <section>
             <div className="wrapper">
                 <ul>
-                    {props.playerSelectTrivia.map((trivia) => {
-                        return(
-                            <li>
-                            <form>
-                                <legend>
-                                   {trivia.question.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'')} 
-                                </legend>
-                                <div><input type="radio" /><label htmlFor=""></label></div>
-                                <div><input type="radio" /><label htmlFor=""></label></div>
-                                <div><input type="radio" /><label htmlFor=""></label></div>
-                                <div><input type="radio" /><label htmlFor=""></label></div>
-                            </form>
-                            </li>
+                    {
+                        props.playerSelectTrivia.map((trivia) => {
+                            let myArray = [...trivia.incorrect_answers];
+                            myArray.push(trivia.correct_answer)
+                    
+                            const shuffle = (array) => {
+                                let currentIndex = array.length, randomIndex;
+                    
+                                while (currentIndex != 0) {
+                    
+                                    randomIndex = Math.floor(Math.random()*currentIndex);
+                                    currentIndex--;
+                    
+                                    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+                                }
+                                return array;
+                            }
+                            shuffle(myArray)
+                            
+                            return( 
+                                <li>
+                                    <form>
+                                        <legend>
+                                        {
+                                        trivia.question.replace(/&quot;/g, '\"').replace(/&#039;/g, '\'')
+                                        } 
+                                        </legend>
+
+                                        <div>
+                                            <input type="radio" />
+                                            <label htmlFor=""></label>{myArray[0]}
+                                        </div>
+
+                                        <div>
+                                            <input type="radio" />
+                                            <label htmlFor=""></label>{myArray[1]}
+                                        </div>
+                                        <div>
+                                            <input type="radio" />
+                                            <label htmlFor=""></label>{myArray[2]}
+                                        </div>
+                                        <div>
+                                            <input type="radio" /><label htmlFor=""></label>{myArray[3]}
+                                        </div>
+                                    </form>
+                                </li>
+                                )
+                            }  
                         )
-                    }  
-                    )}
+                    }
+
                 </ul>
             </div>
         </section>
