cd2404de8c5a4d607024a0fd10a4cb1856e7f568
Kenzie Burke <kenzie8829@gmail.com>
2023-04-06 00:45:18 -0400

    started styling the form and the current game components

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Error from './components/Error';
 
-import {Routes, Route, Link } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
     <Nav />
+    <Home />
     <Main />
 
       <Routes>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,150 +1,149 @@
 // Main.js
-import axios from 'axios';
-import app from './Firebase';
-import { useState, useEffect } from 'react';
-import { getDatabase, ref,  onValue, push } from 'firebase/database';
+import axios from "axios";
+import app from "./Firebase";
+import { useState, useEffect } from "react";
+import { getDatabase, ref, onValue, push } from "firebase/database";
 
-import Form from './Form';
-import CurrentGame from './CurrentGame';
-import Saved from './Saved';
+import Form from "./Form";
+import CurrentGame from "./CurrentGame";
+import Saved from "./Saved";
 
 const shuffle = (array) => {
-    let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
-    while (currentIndex != 0) {
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-        randomIndex = Math.floor(Math.random()*currentIndex);
-        currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+};
 
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-    return array;
-}
-
-let triviaData = { }
+let triviaData = {};
 
 const Main = () => {
-    
-    const [ trivia, setTrivia ] = useState({shuffledData:[], originalData:[]});
-    const [ numQuest, setNumQuest ] = useState(10);
-    const [ questionCategory, setQuestionCategory ] = useState(0);
-    const [ title, setTitle ] = useState('');
+  const [trivia, setTrivia] = useState({ shuffledData: [], originalData: [] });
+  const [numQuest, setNumQuest] = useState(10);
+  const [questionCategory, setQuestionCategory] = useState(0);
+  const [title, setTitle] = useState("");
 
-    const [ savedGames, setSavedGames ] = useState([]);
+  const [savedGames, setSavedGames] = useState([]);
 
-    const handleNumSelection = (e) => {
-        setNumQuest(e.target.value)
-    }
+  const handleNumSelection = (e) => {
+    setNumQuest(e.target.value);
+  };
 
-    const handleCatSelection = (e) => {
-        setQuestionCategory(e.target.value)
-    }
+  const handleCatSelection = (e) => {
+    setQuestionCategory(e.target.value);
+  };
 
-    const handleTitleInput = (e) => {
-        setTitle(e.target.value)
-    }
+  const handleTitleInput = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetchData();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
 
-    useEffect(()=>{
+  useEffect(() => {
+    const db = getDatabase(app);
+    const dbRef = ref(db);
 
-        const db = getDatabase(app);
-        const dbRef = ref(db);
+    onValue(dbRef, (dbGames) => {
+      const dbObj = dbGames.val();
 
-        onValue(dbRef, (dbGames) => {
-            const dbObj = dbGames.val();
+      const arrayOfGames = [];
 
-            const arrayOfGames = [];
+      for (let key in dbObj) {
+        const gameObj = {
+          title: dbObj[key],
+          id: key,
+        };
 
-            for (let key in dbObj) {
-                const gameObj = {
-                    title: dbObj[key],
-                    id: key
-                }
+        arrayOfGames.push(gameObj);
+      }
 
-                arrayOfGames.push(gameObj);
-            }
+      setSavedGames(arrayOfGames);
+    });
+  }, []);
 
-            setSavedGames(arrayOfGames);
-            
-        })
+  //   for(let key in dbObj){
 
-    }, [] )
+  const fetchData = () => {
+    axios({
+      url: "https://opentdb.com/api.php",
+      params: {
+        amount: numQuest,
+        category: questionCategory,
+        type: "multiple",
+      },
+    }).then((apiData) => {
+      // console.log(apiData)
 
-    //   for(let key in dbObj){
+      const shuffledArray = apiData.data.results.map((trivia) => {
+        let myArray = [...trivia.incorrect_answers];
+        myArray.push(trivia.correct_answer);
 
-    const fetchData = ()=>{
+        return shuffle(myArray);
+      });
 
-        axios({
-            url: 'https://opentdb.com/api.php',
-            params: {
-                amount: numQuest,
-                category: questionCategory,
-                type: 'multiple'
-            }
-            
-        }).then((apiData)=>{
-            // console.log(apiData)
+      triviaData = {
+        shuffledData: shuffledArray,
+        originalData: apiData.data.results,
+      };
 
-            const shuffledArray = apiData.data.results.map((trivia) => {
+      console.log(triviaData);
+      setTrivia(triviaData);
 
-                let myArray = [...trivia.incorrect_answers];
-                myArray.push(trivia.correct_answer);
-            
-                return ( shuffle(myArray) )
-            })
+      console.log(trivia);
 
-            triviaData = {shuffledData:shuffledArray,
-            originalData:apiData.data.results}
+      // setTrivia(apiData.data.results)
+      // console.log(apiData.data.results)
 
-            console.log(triviaData)
-            setTrivia(triviaData)
+      // We are pushing straight to firebase after our API call. We will need to (maybe) change this if we want to meet our stretch goal of allowing users to select whether they want to save game.
 
-            console.log(trivia)
-            
+      const db = getDatabase(app);
+      const dbRef = ref(db);
 
-            // setTrivia(apiData.data.results)
-            // console.log(apiData.data.results)
+      const newGame = {
+        userTitle: title,
+        userCategory:
+          apiData.data.results[0].category ===
+            apiData.data.results[1].category &&
+          apiData.data.results[0].category ===
+            apiData.data.results[2].category &&
+          apiData.data.results[0].category === apiData.data.results[3].category
+            ? apiData.data.results[0].category
+            : "Random Questions",
 
-            // We are pushing straight to firebase after our API call. We will need to (maybe) change this if we want to meet our stretch goal of allowing users to select whether they want to save game. 
-            
-            const db = getDatabase(app);
-            const dbRef = ref(db);
+        userGenGame: apiData.data.results,
+      };
 
-            const newGame = { 
-                userTitle: title,
-                userCategory: 
-                    (apiData.data.results[0].category === apiData.data.results[1].category && apiData.data.results[0].category === apiData.data.results[2].category && apiData.data.results[0].category === apiData.data.results[3].category ? apiData.data.results[0].category: 'Random Questions'),
+      push(dbRef, newGame);
+    });
+  };
 
-                userGenGame: apiData.data.results,
+  return (
+    <>
+      <Form
+        handleSubmit={handleSubmit}
+        handleNumSelection={handleNumSelection}
+        handleCatSelection={handleCatSelection}
+        handleTitleInput={handleTitleInput}
+        titleInput={title}
+      />
 
-            }
+      <CurrentGame playerSelectTrivia={trivia} title={title} />
 
-            push(dbRef, newGame);
-
-        })
-
-        
-    }
-
-    return (
-        <>
-            <Form 
-            handleSubmit={handleSubmit}
-            handleNumSelection ={ handleNumSelection }
-            handleCatSelection = { handleCatSelection}
-            handleTitleInput = { handleTitleInput }  
-            titleInput = {title} />
-
-            <Saved 
-            savedGames = {savedGames}/>
-
-            <CurrentGame playerSelectTrivia = {trivia} />
-        </>
-    )
-}
+      <Saved savedGames={savedGames} />
+    </>
+  );
+};
 
 export default Main;
