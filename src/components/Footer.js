8668a39fd6947ac8f8b4a39fee98c97521e586f7
Kenzie Burke <kenzie8829@gmail.com>
2023-04-11 20:11:43 -0400

    changed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <p>Created by Denny Selda Pedram & Kenzie</p>
+      <p>Created by <a href="https://daeheumna.com/">Denny</a> <a href="https://seldaerkilic.com/">Selda</a> <a href="https://pedramcodes.com/">Pedram</a> & <a href="https://kenzieburke.ca">Kenzie</a> <a href="https://junocollege.com/">Juno College.</a></p>
     </footer>
   );
 };
